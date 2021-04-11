b8daa23f1e2d28eceade7e8850a119c0931b3b73
Youheng Hou <houyouheng@hotmail.com>
2021-04-11 20:15:14 +0100

    added console communication

--- main.py
+++ main.py
@@ -1,23 +1,12 @@
 import RPi.GPIO as GPIO          
 from time import sleep
 
-ser = serial.Serial(
-        port='/dev/ttyS0',
-        baudrate = 9600,
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.EIGHTBITS,
-        timeout=1
-)
+startMsg = "Please specify the motor and angle \n Example: \"1 30\", or \"2 60\""
+print(startMsg)
 
-
-print("\n")
-print("Please specify the motor and angle")
-print("Example: \"1 30\" or \"2 60\"")
-print("\n")    
 
 while(1):
-        message = ser.readline()
-        print(message)
+        message = input()
+        print("You have inputed: " + message)
 
     
