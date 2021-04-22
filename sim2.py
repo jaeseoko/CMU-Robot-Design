f1850f0cd8d52c32ecd167289160c1ecde0fcdea
Jae O <jaeseoko@Jaes-MacBook-Pro.local>
2021-04-23 04:05:40 +0900

    updated sim2

--- sim2.py
+++ sim2.py
@@ -7,23 +7,27 @@ import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument('x', 
-        type=float,
-        help='1st target end effector world coordinate x')
+                    type=float,
+                    help='1st target end effector world coordinate x')
 parser.add_argument('y', 
-        type=float,
-        help='1st target end effector world coordinate y')
+                    type=float,
+                    help='1st target end effector world coordinate y')
 parser.add_argument('z', 
-        type=float,
-        help='1st target end effector world coordinate z')
+                    type=float,
+                    help='1st target end effector world coordinate z')
+
+parser.add_argument('--weight',
+                    type = float,
+                    help = 'set payload weight to pick up')
 args = parser.parse_args()
 
 target = [args.x,args.y,args.z]
 
-destination = [0.4,-0.4,args.z]
+destination = [args.x,-args.y,args.z]
 
-offsetJoint3 = -5*np.pi/180
+offsetJoint3 = -10*np.pi/180
 
-tol = 1e-3
+tol = 1e-2
 
 # Can alternatively pass in p.DIRECT 
 client = p.connect(p.GUI)
@@ -37,10 +41,7 @@ planeId = p.loadURDF("plane.urdf")
 
 ''' LOAD '''
 flags = p.URDF_USE_SELF_COLLISION
-# bodyId = p.loadURDF("./Arm_Final_Planet/urdf/Arm_Final_Planet.urdf", 
-#                     basePosition=[0,0,0],useFixedBase=True,flags=flags)
-# bodyId = p.loadURDF("./data/version2/Arm_Final_Planet/urdf/Arm_Final_Planet.urdf",
-#                     basePosition=[0,0,0],useFixedBase=True,flags=flags)
+
 bodyId = p.loadURDF("./data/Arm_Final_Planet_hook/urdf/Arm_Final_Planet_hook.urdf",
                     basePosition=[0,0,0],useFixedBase=True,flags=flags)
                     
@@ -54,10 +55,6 @@ print("number of joints = ",numJoints)
 #     print("Joint ", i, "\n")
 #     print(p.getJointInfo(bodyId,i))
 
-mode = p.VELOCITY_CONTROL
-maxForce = 10
-targetVel = 4
-
 
 
 
@@ -67,6 +64,7 @@ torqueLog0 = []
 torqueLog1 = []
 torqueLog2 = []
 errorLog0 = []
+minE = []
 errorLog1 = []
 errorLog2 = []
 prev_error0 = 0
@@ -76,21 +74,23 @@ cumul_e0 = 0
 cumul_e1 = 0
 cumul_e2 = 0
 
+# PARAMS
 kp0 = 2e-2
 ki0 = 1e-8
-kd0 = 4e02
+# kd0 = 400/240
+kd0 = 2e-2
 
 kp1 = 3e-2
 ki1 = 1e-7
-kd1 = 5e02
+kd1 = 4e-2
 
 kp2 = 2e-2
 ki2 = 1e-4
-kd2 = 4e02
+kd2 = 2e-2
 
-print("0 -> kp,ki,ki = ",kp0,ki0,kd0)
-print("1 -> kp,ki,ki = ",kp1,ki1,kd1)
-print("2 -> kp,ki,ki = ",kp2,ki2,kd2)
+# print("0 -> kp,ki,kd = ",kp0,ki0,kd0)
+# print("1 -> kp,ki,kd = ",kp1,ki1,kd1)
+# print("2 -> kp,ki,kd = ",kp2,ki2,kd2)
 
 prevPose = [0, 0, 0]
 prevPose1 = [0, 0, 0]
@@ -100,57 +100,70 @@ hasPrevPose = 0
 p.enableJointForceTorqueSensor(bodyId,0)
 p.enableJointForceTorqueSensor(bodyId,1)
 p.enableJointForceTorqueSensor(bodyId,2)
-maxForce = 0
 
-mode = p.VELOCITY_CONTROL 
-p.setJointMotorControl2(bodyId, 0,controlMode=mode, force=maxForce)
-p.setJointMotorControl2(bodyId, 1,controlMode=mode, force=maxForce)
-p.setJointMotorControl2(bodyId, 2,controlMode=mode, force=maxForce)
+
+
+maxForce = 0
+p.setJointMotorControl2(bodyId, 0,controlMode=p.VELOCITY_CONTROL, force=maxForce)
+p.setJointMotorControl2(bodyId, 1,controlMode=p.VELOCITY_CONTROL, force=maxForce)
+p.setJointMotorControl2(bodyId, 2,controlMode=p.VELOCITY_CONTROL, force=maxForce)
 
 ind,Name,tp,qInd,uInd,flags,damp, \
 friction,ll,ul,maxF,maxV,linkName, \
 ax,parPos,parOrn,ParInd = p.getJointInfo(bodyId,0)
 # print("friction = ",friction)
 
-p.changeDynamics(bodyId,0,jointLowerLimit = -np.pi,jointUpperLimit = np.pi)
-p.changeDynamics(bodyId,1,jointLowerLimit = -np.pi,jointUpperLimit = np.pi)
-p.changeDynamics(bodyId,2,jointLowerLimit = -np.pi,jointUpperLimit = np.pi)
+p.changeDynamics(bodyId,0, rollingFriction = 1e-5, spinningFriction = 1e-4)
+p.changeDynamics(bodyId,1,jointLowerLimit = -np.pi,jointUpperLimit = np.pi, rollingFriction = 1e-5,spinningFriction = 1e-2)
+p.changeDynamics(bodyId,2,jointLowerLimit = -np.pi,jointUpperLimit = np.pi, rollingFriction = 1e-5,spinningFriction = 1e-2)
 
 
 ee_mass = p.getDynamicsInfo(bodyId,3)[0]
 print("mass of end effector : ", ee_mass)
-# print(p.getLinkState(bodyId,-1))
 
 
-# print(p.getLinkState(bodyId,0))
-# print(p.getLinkState(bodyId,1))
-# print(p.getLinkState(bodyId,2))
-# print("--------")
-# print("--------")
-# print("--------")
-# print(p.getLinkState(bodyId,3))
+# tarOrn = p.getQuaternionFromEuler([np.pi/2,np.pi/2,-np.pi/2])
+# desOrn = p.getQuaternionFromEuler([0,0,0])
 
 
-targetORN = [np.pi/2,-np.pi/4,-np.pi/2]
-# targetORN = [0,0,0]
+targetORN = np.asarray( p.calculateInverseKinematics(bodyId,3,target) )
+                        # lowerLimits = [-np.pi, -np.pi/2,-np.pi/2],
+                        # upperLimits = [np.pi, 0,0]  ) )
 
-# target = [0.4,0.2,0.1]
+destORN = np.asarray( p.calculateInverseKinematics(bodyId,3,destination) )
+                        # lowerLimits = [-np.pi, -np.pi/2,-np.pi/2],
+                        # upperLimits = [np.pi, 0,0]  ) )
 
-jointPos = p.calculateInverseKinematics(bodyId,3,target)
 
-destPos = p.calculateInverseKinematics(bodyId,3,destination)
 
-destination = destPos
+if(targetORN[1]>0):
+    
+    targetORN[1]*= -1
+    print("inverting target joint 1 angle")
+    targetORN[0]+=np.pi
+    print("180 target rotating joint 0")
+if(targetORN[2]>0):
+    print("inverting target joint 2 angle")
+    targetORN[2]*=-1
 
-if(target[0]<0):
-    targetORN = [-np.pi +jointPos[0],-jointPos[1],-jointPos[2]]
-else:
-    targetORN = [np.pi +jointPos[0],-jointPos[1],-jointPos[2]]
+if(destORN[1]>0):
+    
+    destORN[1]*= -1
+    print("inverting dest joint 1 angle")
+    destORN[0]+=np.pi
+    print("180 dest rotating joint 0")
+if(destORN[2]>0):
+    print("inverting dest joint 2 angle")
+    destORN[2]*=-1
 
 
 print("target pos 0 (deg.):",targetORN[0]*180/np.pi)
 print("target pos 1 (deg.):",targetORN[1]*180/np.pi)
 print("target pos 2 (deg.):",targetORN[2]*180/np.pi)
+
+print("destination pos 0 (deg.):",destORN[0]*180/np.pi)
+print("destination pos 1 (deg.):",destORN[1]*180/np.pi)
+print("destination pos 2 (deg.):",destORN[2]*180/np.pi)
 
 state1 = False
 a1 = False
@@ -158,13 +171,15 @@ state2 = False
 a2 = False
 state3 = False
 
-payload = 0.1
+payload = args.weight
 
-duration = 50000
+duration = 30000
 
 
 p.setRealTimeSimulation(0)
 t0 = time.time()
+dt = 1/240
+termTime = 0
 for i in range(duration): 
     
     # pos, ori = p.getBasePositionAndOrientation(bodyId)
@@ -178,7 +193,7 @@ for i in range(duration):
         a2 = True
         print("Attempting at state 2 . . .")
     elif state2==True:
-        targetORN = destination
+        targetORN = destORN
 
 
     pos = [0,0,0]
@@ -198,18 +213,18 @@ for i in range(duration):
     pos0,vel0,RF0,torque0 = p.getJointState(bodyId,0)
     pos1,vel1,RF1,torque1 = p.getJointState(bodyId,1)
     pos2,vel2,RF2,torque2 = p.getJointState(bodyId,2)
-    # tau0,tau1,tau2 = p.calculateInverseDynamics(bodyId,
-    #                                             [pos0,pos1,pos2],
-    #                                             [vel0,vel1,vel2],
-    #                                             [0,0,0])
     tau0,tau1,tau2 = p.calculateInverseDynamics(bodyId,
                                                 [pos0,pos1,pos2],
-                                                [0,0,0],
+                                                [vel0,vel1,vel2],
                                                 [0,0,0])
+    # tau0,tau1,tau2 = p.calculateInverseDynamics(bodyId,
+    #                                             [pos0,pos1,pos2],
+    #                                             [0,0,0],
+    #                                             [0,0,0])
 
-    T0 = kp0*(error0) + kd0*(de0/240) + ki0*cumul_e0
-    T1 = kp1*(error1) + kd1*(de1/240) + ki1*cumul_e1
-    T2 = kp2*(error2) + kd2*(de2/240) + ki2*cumul_e2
+    T0 = kp0*(error0) + kd0*(de0/dt) + ki0*cumul_e0
+    T1 = kp1*(error1) + kd1*(de1/dt) + ki1*cumul_e1
+    T2 = kp2*(error2) + kd2*(de2/dt) + ki2*cumul_e2
     # print("torques 0 1 2: ",T0,",",T1,",",T2)
 
     prev_error0 = error0
@@ -223,6 +238,7 @@ for i in range(duration):
     ####
     
     ####
+    # tau0 = 0
     force0 = T0 + tau0
     force1 = T1 + tau1
     force2 = T2 + tau2
@@ -230,12 +246,7 @@ for i in range(duration):
     p.setJointMotorControl2(bodyId,0,controlMode = p.TORQUE_CONTROL, force = force0)
     p.setJointMotorControl2(bodyId,1,controlMode = p.TORQUE_CONTROL, force = force1)
     p.setJointMotorControl2(bodyId,2,controlMode = p.TORQUE_CONTROL, force = force2)
-    # p.setJointMotorControl2(bodyId,0,controlMode = p.POSITION_CONTROL, targetPosition = targetORN[0])
-    # p.setJointMotorControl2(bodyId,1,controlMode = p.POSITION_CONTROL, targetPosition = targetORN[1])
-    # p.setJointMotorControl2(bodyId,2,controlMode = p.POSITION_CONTROL, targetPosition = targetORN[2])
-    # _,_,_,force0 = p.getJointState(bodyId,0)
-    # _,_,_,force1 = p.getJointState(bodyId,0)
-    # _,_,_,force2 = p.getJointState(bodyId,0)
+    
 
     errorLog0.append(error0*180/np.pi)
     errorLog1.append(error1*180/np.pi)
@@ -243,6 +254,8 @@ for i in range(duration):
     torqueLog0.append(force0)
     torqueLog1.append(force1)
     torqueLog2.append(force2)
+    minE.append(False)
+    
 
     if (np.abs(v0)+np.abs(v1)+np.abs(v2)) <tol and \
        (np.abs(error0)+np.abs(error1)+np.abs(error2)) <tol:
@@ -250,33 +263,49 @@ for i in range(duration):
             state1 = True
             print("STATE CHANGED")
             print("reached state 1")
-            cumul_e0 = 0
-            cumul_e1 = 0
-            cumul_e2 = 0
+            minE[-1] = True
+            
+            # cumul_e0 = 0
+            # cumul_e1 = 0
+            # cumul_e2 = 0
         elif state2==False: 
             state2 = True
             print("STATE CHANGED")
             print("reached state 2")
-            cumul_e0 = 0
-            cumul_e1 = 0
-            cumul_e2 = 0
+            # cumul_e0 = 0
+            # cumul_e1 = 0
+            # cumul_e2 = 0
             p.changeDynamics(bodyId,3,mass = ee_mass+payload)
-            # kp1*=3e03*payload
-            # ki1*=3e03*payload
-            # kd1*=3e03*payload
-            # kp2*=1e02*payload
-            # ki2*=1e02*payload
-            # kd2*=1e02*payload
-            kp1+=8e-01*payload
-            ki1+=8e-01*payload
-            kd1+=8e-01*payload
-            kp2+=1e-03*payload
-            ki2+=1e-03*payload
-            kd2+=1e-03*payload
+            minE[-1] = True
+
+            # kp0*=1e-03*payload
+            # ki0*=1e-01*payload
+            # kd0*=5e-03*payload
+
+            # kp1*=1e02*payload
+            # ki1*=1e03*payload
+            # kd1*=1e05*payload
+
+            # kp2*=1e03*payload
+            # ki2*=1e03*payload
+            # kd2*=1e03*payload
+
+            kp0+=1e-04*payload
+            ki0+=1e-04*payload
+            kd0+=1e-04*payload
+            kp1+=9*payload
+            ki1+=8e-02*payload
+            kd1+=9*payload
+            kp2+=1e-04*payload
+            ki2+=1e-04*payload
+            kd2+=1e-04*payload
         else: 
             state3 = True
             print("reached state 3")
-            t1 = time.time()
+            minE[-1] = True
+            termTime=i+1
+            break
+            # t1 = time.time()
             
         
 
@@ -288,22 +317,32 @@ for i in range(duration):
             print("current errors (deg.): ",error0*180/np.pi,", ",
                                             error1*180/np.pi,", ",
                                             error2*180/np.pi)
+    termTime = i+1
     p.stepSimulation()
     
-print("total time took (real time sim) : ", t1-t0)
+# print("total time took (real time sim) : ", t1-t0)
     
+
+
+
+''' PLOT ''' ''' PLOT ''' ''' PLOT ''' ''' PLOT ''' ''' PLOT ''' ''' PLOT ''' ''' PLOT '''
+
+
+
 # PLOT
 
 figure = plt.figure(figsize=[15, 4.5])
 figure.subplots_adjust(left=0.05, bottom=0.11, right=0.97, top=0.9, wspace=0.4, hspace=0.55)
 
-t = np.linspace(0,duration/240,duration)
+t = np.linspace(0,termTime/240,termTime)
 torqueLog0 = np.asarray(torqueLog0)
 torqueLog1 = np.asarray(torqueLog1)
 torqueLog2 = np.asarray(torqueLog2)
 errorLog0 = np.asarray(errorLog0)
 errorLog1 = np.asarray(errorLog1)
 errorLog2 = np.asarray(errorLog2)
+minE = np.asarray(minE)
+
 
 ax_1 = figure.add_subplot(131)
 ax_1.set_title("TORQUE 0")
@@ -330,9 +369,13 @@ ax_3.set_ylabel("NM")
 figure = plt.figure(figsize=[15, 4.5])
 figure.subplots_adjust(left=0.05, bottom=0.11, right=0.97, top=0.9, wspace=0.4, hspace=0.55)
 
+mask = minE
+
+
 ax_4 = figure.add_subplot(131)
 ax_4.set_title("joint 0 error")
 ax_4.plot(t, errorLog0, '-r')
+ax_4.scatter(t[mask],errorLog0[mask],c='k')
 ax_4.grid()
 ax_4.set_xlabel("seconds")
 ax_4.set_ylabel("degrees")
@@ -340,6 +383,7 @@ ax_4.set_ylabel("degrees")
 ax_5 = figure.add_subplot(132)
 ax_5.set_title("joint 1 error")
 ax_5.plot(t, errorLog1, '-g')
+ax_5.scatter(t[mask],errorLog1[mask],c='k')
 ax_5.grid()
 ax_5.set_xlabel("seconds")
 ax_5.set_ylabel("degrees")
@@ -347,6 +391,7 @@ ax_5.set_ylabel("degrees")
 ax_6 = figure.add_subplot(133)
 ax_6.set_title("joint 2 error")
 ax_6.plot(t, errorLog2, '-b')
+ax_6.scatter(t[mask],errorLog2[mask],c='k')
 ax_6.grid()
 ax_6.set_xlabel("seconds")
 ax_6.set_ylabel("degrees")
