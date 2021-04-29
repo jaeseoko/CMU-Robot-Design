6170121aa1824f55888cd42fb58b2d5d651637c1
Youheng Hou <houy@rose-hulman.edu>
2021-04-29 19:37:37 -0400

    100

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ prev_pos = 0
 GPIO.output(motor_driver_1_reverse_enable_pin, GPIO.HIGH)
 GPIO.output(motor_driver_1_forward_enable_pin, GPIO.HIGH)
 
-motor_driver_1_forward_pwm.start(2)
+motor_driver_1_forward_pwm.start(100)
 # motor_driver_1_reverse_pwm.start(50)
 
 
