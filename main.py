6c84c6d6df65efff3ae1e1eee17b151a28fcdc43
Youheng Hou <houy@rose-hulman.edu>
2021-04-29 20:42:21 -0400

    /3

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ def main():
     global prev_pos
 
     pos0 = (motor_1_encoder.read()) / (encoder_count_per_rotation)  # rad
-    vel0 = (pos0 - prev_pos) / (dt) * 60# rad/s
+    vel0 = (pos0 - prev_pos) / (dt) * 60 / 3# rad/s
 
     print("Raw encoder data: " + str(motor_1_encoder.read()) + ". position: " + str(pos0) + ". velocity: " + str(vel0) + ".")
 
