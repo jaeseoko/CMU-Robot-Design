8584fa7c01aa2c8f79800c8a3e1b631d0d406493
Youheng Hou <houy@rose-hulman.edu>
2021-05-03 12:14:02 -0400

    change motor

--- main_test.py
+++ main_test.py
@@ -116,12 +116,12 @@ V = 12
 
 # GPIOs--------------------------------------
 # First Motor related
-motor_driver_1_reverse_enable_pin = 4       # GPIO 4
-motor_driver_1_forward_enable_pin = 17      # GPIO 17
-motor_driver_1_reverse_pwm_pin = 27         # GPIO 27
-motor_driver_1_forward_pwm_pin = 22         # GPIO 22
-motor_1_Encoder_A_pin = 18                  # GPIO 18
-motor_1_Encoder_B_pin = 23                  # GPIO 23
+motor_driver_1_reverse_enable_pin = 6       # GPIO 4
+motor_driver_1_forward_enable_pin = 13      # GPIO 17
+motor_driver_1_reverse_pwm_pin = 19         # GPIO 27
+motor_driver_1_forward_pwm_pin = 26         # GPIO 22
+motor_1_Encoder_A_pin = 12                  # GPIO 18
+motor_1_Encoder_B_pin = 16                  # GPIO 23
 
 # Second Motor related
 motor_driver_2_reverse_enable_pin = 10      # GPIO 10
@@ -132,12 +132,12 @@ motor_2_Encoder_A_pin = 24                  # GPIO 24
 motor_2_Encoder_B_pin = 25                  # GPIO 25
 
 # Third Motor related
-motor_driver_3_reverse_enable_pin = 6       # GPIO 6
-motor_driver_3_forward_enable_pin = 13      # GPIO 13
-motor_driver_3_reverse_pwm_pin = 19         # GPIO 19
-motor_driver_3_forward_pwm_pin = 26         # GPIO 26
-motor_3_Encoder_A_pin = 12                  # GPIO 12
-motor_3_Encoder_B_pin = 16                  # GPIO 16
+motor_driver_3_reverse_enable_pin = 4       # GPIO 6
+motor_driver_3_forward_enable_pin = 17      # GPIO 13
+motor_driver_3_reverse_pwm_pin = 27         # GPIO 19
+motor_driver_3_forward_pwm_pin = 22         # GPIO 26
+motor_3_Encoder_A_pin = 18                  # GPIO 12
+motor_3_Encoder_B_pin = 23                  # GPIO 16
 
 # GPIO initialization--------------------------------------
 GPIO.setmode(GPIO.BCM)
