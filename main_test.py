ea1268e6bb9618886fe68c8b6630565766dc2fbd
Jae O <jaeseoko@Jaes-MacBook-Pro.local>
2021-05-05 05:33:47 +0900

    pid

--- main_test.py
+++ main_test.py
@@ -84,9 +84,9 @@ def PID_torque(e,de,cum_e,load):
     # kp0,ki0,kd0 = 2e-2, 1e-8 , 2e-2
     kp0,ki0,kd0 = 9e-2, 1e-8 , 9e-2
     # kp1,ki1,kd1 = 3e-2, 1e-7 , 4e-2
-    kp1,ki1,kd1 = 9e-1, 1e-5 , 9e-1
+    kp1,ki1,kd1 = 9, 1e-4 , 9
     # kp2,ki2,kd2 = 2e-2, 1e-4 , 2e-2
-    kp2,ki2,kd2 = 9e-1, 1e-4 , 9e-1
+    kp2,ki2,kd2 = 9, 1e-3 , 9
 
 
     if(load!=0):
