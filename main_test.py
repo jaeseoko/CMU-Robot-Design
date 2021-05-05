ec1d091c71c06eed9cae68b17e22c864049e7e41
Jae O <jaeseoko@Jaes-MacBook-Pro.local>
2021-05-06 07:29:26 +0900

    adas

--- main_test.py
+++ main_test.py
@@ -44,7 +44,8 @@ def SetUp():
     args = parser.parse_args()
     targetORN = [args.a0*np.pi/180,args.a1*np.pi/180,args.a2*np.pi/180]
     destORN = [args.a0*np.pi/180 + np.pi/2,args.a1*np.pi/180,args.a2*np.pi/180]
-    prev_pos = [0,-(85)*np.pi/180,0]
+    # prev_pos = [0,-(85)*np.pi/180,0]
+    prev_pos = [0,0,0]
     prev_error = [0,0,0]
     cum_e = [0,0,0]
     load = args.load
