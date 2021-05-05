876277d04db64f37f5ba9e351a9a756ed7008dd5
Jae O <jaeseoko@Jaes-MacBook-Pro.local>
2021-05-06 07:27:56 +0900

    adas

--- main_test.py
+++ main_test.py
@@ -85,9 +85,9 @@ def PID_torque(e,de,cum_e,load):
     # kp0,ki0,kd0 = 2e-2, 1e-8 , 2e-2
     kp0,ki0,kd0 = 9e-2, 1e-8 , 9e-2
     # kp1,ki1,kd1 = 3e-2, 1e-7 , 4e-2
-    kp1,ki1,kd1 = 1.5, 1e-3 , 2.0
+    kp1,ki1,kd1 = 4.5, 1e-3 , 4.0
     # kp2,ki2,kd2 = 2e-2, 1e-4 , 2e-2
-    kp2,ki2,kd2 = 9e-1, 1e-3 , 9e-1
+    kp2,ki2,kd2 = 10e-1, 1e-3 , 10e-1
 
 
     if(load!=0):
@@ -336,14 +336,14 @@ def main():
 
     if(volt[0]>0): rotateCW(0, abs(volt[0]))
     else: rotateCCW(0, abs(volt[0]))
-    if(volt[1]>0): rotateCW(1, abs(10*volt[1]))
-    else: rotateCCW(1, abs(10*volt[1]))
+    if(volt[1]>0): rotateCW(1, abs(volt[1]))
+    else: rotateCCW(1, abs(volt[1]))
     if picked==True and worm == True:
         stopRotate(2)
     elif(volt[2]>0): 
-        rotateCW(2, abs(10*volt[2]))
+        rotateCW(2, abs(volt[2]))
     else: 
-        rotateCCW(2, abs(10*volt[2]))
+        rotateCCW(2, abs(volt[2]))
 
 
     print("position 0: " + str(pos[0]) + ". velocity 0: " + str(vel[0]) + ".")
