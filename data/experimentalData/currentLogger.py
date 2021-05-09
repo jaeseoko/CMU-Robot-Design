b1f14e953f1d77f67f9a75ea599ab3ffc0fcaab9
Youheng Hou <houy@rose-hulman.edu>
2021-05-09 15:06:35 -0400

    added codes foe 2 DMMs

--- data/experimentalData/currentLogger.py
+++ data/experimentalData/currentLogger.py
@@ -3,37 +3,61 @@ import time
 from time import sleep
 import numpy as np
 
+
 rm = visa.ResourceManager()
 print('Connected VISA resources:')
 print(rm.list_resources())
 
-dmm = rm.open_resource('USB0::0x1AB1::0x0C94::DM3O191900311::INSTR')
-print('Instrument ID (IDN:) = ', dmm.query('*IDN?'))
+dmm = rm.open_resource('USB0::0x1AB1::0x0C94::DM3O171700266::INSTR')
+dmm2 = rm.open_resource('USB0::0x1AB1::0x0C94::DM3O191900311::INSTR')
+# print('Instrument ID (IDN:) = ', dmm.query('*IDN?'))
 #print("Volts DC   = ", dmm.query(":MEASure:VOLTage:DC?"))
-print("DC Current = ", dmm.query(":MEASure:CURRent:DC?"))
-
-print("Poll rate = 500mS. Will run for 24 hours collecting 172,800 readings")
-print("output file = iLog.csv\n\n")
+# print("DC Current = ", dmm.query(":MEASure:CURRent:DC?"))
 print(" Seconds Count    ", "DC Current", "Raw Meter Response", sep="\t|\t")
 print("----------------------------------------------------------------------------------")
 
 start_time = time.time()
 
-for x in range(0, 100):
+
+data_points = 20
+
+
+current_data = np.zeros((data_points, 2))
+current_data2 = np.zeros((data_points, 2))
+
+for x in range(0, data_points):
     rawStr = dmm.query(":MEASure:CURRent:DC?")
+
     iStr = rawStr
     rawStr = rawStr.replace ("\n", "") 
     iStr = iStr.replace("\n", "")
     iStr = iStr.replace("#9000000015", "")
 
     iFlt = float(iStr)
+
+    rawStr2 = dmm2.query(":MEASure:CURRent:DC?")
+    # print(rawStr)
+
+    iStr2 = rawStr2
+    rawStr2 = rawStr2.replace ("\n", "") 
+    iStr2 = iStr2.replace("\n", "")
+    iStr2 = iStr2.replace("#9000000015", "")
+
+    iFlt2 = float(iStr2)
+
+
     now = time.time() - start_time
 
     current_data[x][0] = now
     current_data[x][1] = iFlt
 
+    current_data2[x][0] = now
+    current_data2[x][1] = iFlt2
+
     print(now, iFlt ,rawStr, sep="\t|\t")
+    print(now, iFlt2 ,rawStr, sep="\t|\t")
 
-    sleep(.03)
+    # sleep(.01)
 
-numpy.savetxt("currentData.csv", a, delimiter=',', header="Time (Seconds),Current (Amps)", comments="")
+np.savetxt("currentData.csv", current_data, delimiter=',', header="Time (Seconds),Current (Amps)", comments="")
+np.savetxt("currentData2.csv", current_data2, delimiter=',', header="Time (Seconds),Current (Amps)", comments="")
